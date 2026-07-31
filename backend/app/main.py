9717b26996f19f1a8aa8b4f0fa41906f2decd091
Vanshika Bhosale <vanshikabhosale22@gmail.com>
2026-07-31 14:09:38 +0530

    frontend dashboard

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,3 +1,4 @@
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI
 from app.database import Base, engine
 
@@ -58,6 +59,16 @@ from app.routers import (
 app = FastAPI(
     title="InsightIQ AI Executive Analytics API",
     version="1.0.0"
+)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 # ==========================
